bfe07d3b850593bff9b281a8f7a9322eb0906bcb
Guilherme Silva <guisilva2110@gmail.com>
2021-06-23 02:52:51 -0300

    Versão Game 1.1 / Criação da Tela do jogo

--- game.py
+++ game.py
@@ -1,4 +1,30 @@
 import pygame
-import time
-import random
+from obj import Obj
+
+class Main:
+
+    def __init__(self, sizex, sizey, title):
+
+        self.window = pygame.display.set_mode([sizex, sizey])
+        self.title = pygame.display.set_caption(title)
+
+        self.loop = True
+
+    def draw(self):
+        pass
+    
+    def events(self):
+        for events in pygame.event.get():
+            if events.type == pygame.QUIT:
+                self.loop = False
+
+    def update(self):
+        while self.loop:
+            self.draw()
+            self.events()
+            pygame.display.update()
+
+
+game = Main (320, 640, "My Hero Academia")
+game.update()
 
